6af3bd820cc8348f95378d71d3ef036482c2b873
Zilya Karimova <zilja081295@mail.ru>
2021-06-02 17:12:04 +0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    private static final String URL = "jdbc:postgresql://localhost/postgres?user=postgres&password=KIslam18*Aza";
+    private static String conok="Соединение с бд установлено";
+    private static String conerr="Произошла ошибка подключения к бд";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(URL)){
+            System.out.println(String.format("%s",conok));
+        } catch (SQLException e) {
+            System.out.println(String.format("%s",conerr));
+            e.printStackTrace();
+        }
+    }
+}
